9331182f8b8d0d39e2d43e6366dbc66f55da8135
imlautaro <dev.lautaropereyra@gmail.com>
2021-03-09 15:59:43 +0000

    Deploying to gh-pages from @ imlautaro/cats-game@5da536cb8fff4b136a10bffb24dae760d91a5322 🚀

---
+++ _nuxt/static/1615305582/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcats-game\u002F_nuxt\u002Fstatic\u002F1615305582",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fcats-game\u002F",assetsPath:"\u002Fcats-game\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
